7ec32289b64089c3944beae9731a03053214203d
ariel <ariel.pas.diaz@gmail.com>
2025-01-26 12:52:59 +0100

    interceptor añade accept application/json

--- src/app/interceptors/auth-token.interceptor.ts
+++ src/app/interceptors/auth-token.interceptor.ts
@@ -5,7 +5,10 @@ import { inject } from '@angular/core';
 export const AuthTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(IAuthenticationService);
   const reqWithAuth = req.clone({
-    headers : req.headers.set('Authorization' , `Bearer ${authService.token()}`)
+    headers : req.headers
+      .append('Authorization' , `Bearer ${authService.token()}`)
+      .append('Accept', 'application/json')
+
   })
 
   return authService.token() ? next(reqWithAuth) : next(req);
