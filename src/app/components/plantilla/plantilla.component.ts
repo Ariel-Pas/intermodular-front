1f794fbeaa626401bbaed197b258926ef31fa66e
ihanfmxmvcz <ihanfmxmvcz@proton.me>
2025-02-09 22:11:10 +0100

    Vista Login

--- src/app/components/plantilla/plantilla.component.ts
+++ src/app/components/plantilla/plantilla.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { PlantillaNavComponent } from '../plantilla-nav/plantilla-nav.component';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-plantilla',
-  imports: [PlantillaNavComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [PlantillaNavComponent, RouterOutlet, RouterLink, RouterLinkActive, AsyncPipe],
   templateUrl: './plantilla.component.html',
   styleUrl: './plantilla.component.scss'
 })
 export class PlantillaComponent {
+
+  //Behavior Subject es un observable con un estado inicial, que reacciona a cambios y permite obtener el ultimo estado.
+  currentRoute = new BehaviorSubject<string>('');
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute.next(this.router.url);
+    });
+  }
+
+
 
 }
